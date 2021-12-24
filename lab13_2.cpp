a2a4aae1441c8df51c06d8334f2d9fdcc5314e95
Surabordin <bluelimec@gmail.com>
2021-12-24 16:07:47 +0700

    complete

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int x , int y);
+
 int main(){
     int x, y;
+    x = 15;
+    y = 25;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+
+int gcd(int x , int y){
+    if (x%y == 0) return y;
+    else return gcd(y,x%y);
+    
 }
